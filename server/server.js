40ed140d48f5039c91e05111944306965824de40
Greg <you@example.com>
2019-01-24 16:09:09 +0000

    Add test cases for GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -46,7 +46,7 @@ app.get('/todos/:id', (req, res) => {
     }
     Todo.findById(id).then((todo) => {
         if (!todo) {
-            return res.status(400).send('Can\'t find user with ID entered.');
+            return res.status(404).send('Can\'t find user with ID entered.');
         }
         res.status(200).send({todo})
     }, (err) => { console.log(err) }).catch((err) => {
